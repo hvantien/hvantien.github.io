1ebe8e33d32f337e53751ea3f2dfbebd25ba5577
tienhoang <tien.hoang@mainbridgehp.com>
2024-09-16 16:00:46 +0700

    update

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let touchStartY = 0;
 let touchEndY = 0;
 let touchStartTime = 0;
 let touchEndTime = 0;
-const SCROLL_THRESHOLD = 20; // Khoảng cách tối thiểu để coi là cuộn
-const TIME_THRESHOLD = 50; // Thời gian tối đa để coi là nhấn (ms)
+const SCROLL_THRESHOLD = 50; // Khoảng cách tối thiểu để coi là cuộn
+const TIME_THRESHOLD = 150; // Thời gian tối đa để coi là nhấn (ms)
 
 const moreButton = document.querySelector('.more-button');
 
